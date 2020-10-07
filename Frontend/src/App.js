4f3d0e71f3841239179c58c6465cc0ca53d93275
Cristiano Lima <52179531+K16bits@users.noreply.github.com>
2020-10-07 20:42:53 -0300

    Começando o front

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,39 +1,9 @@
-import React, { useState } from 'react';
-import Container from './Components/Container'
-import Input from './Components/Input'
-import Button from './Components/Button'
-
+import React from 'react';
+import Routes from "./routes"
 
 function App() {
-
-  const [Nome,setNome] = useState("");
-  const [Senha,setSenha] = useState("");
-
-  async function HandleSubmit(e){
-    e.preventDefault();
-
-    const data = {
-      Nome,
-      Senha
-    }
-    console.log(data);
-  }
-
-  return (
-    <Container>
-      <form onSubmit={HandleSubmit}>
-        <Input name="Nome" placeholder="Email"
-          onChange = { e => setNome(e.target.value) }
-        ></Input>
-
-        <Input name="Senha" type="password" placeholder="Senha"
-          onChange = { e => setSenha(e.target.value)}
-        ></Input>
-        <Button type="submit">Entrar</Button>
-        <span>Criar conta</span>
-      </form>
-      
-    </Container>
+  return(
+    <Routes/>
   );
 }
 
---
+++ Frontend/src/pages/Home/index.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import Container from '../../Components/Container'
+import Input from '../../Components/Input'
+import Button from '../../Components/Button'
+
+
+export default function Home() {
+
+  const [Nome,setNome] = useState("");
+  const [Senha,setSenha] = useState("");
+
+  async function HandleSubmit(e){
+    e.preventDefault();
+
+    const data = {
+      Nome,
+      Senha
+    }
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <form onSubmit={HandleSubmit}>
+        <Input name="Nome" placeholder="Email" autoComplete='off'
+          onChange = { e => setNome(e.target.value) }
+        ></Input>
+
+        <Input name="Senha" type="password" placeholder="Senha"
+          onChange = { e => setSenha(e.target.value)}
+        ></Input>
+        <Button type="submit">Entrar</Button>
+        <span>Criar conta</span>
+      </form>
+      
+    </Container>
+  );
+}
