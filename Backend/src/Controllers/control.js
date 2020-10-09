e8ab3b648b41db374bbc678b7382e70935389d5c
Cristiano Lima <52179531+K16bits@users.noreply.github.com>
2020-10-09 14:39:42 -0300

    teste

--- Backend/src/Controllers/control.js
+++ Backend/src/Controllers/control.js
@@ -3,23 +3,30 @@ const conection = require('../Data/conection');
 module.exports = {
     async data(req,res){
         const {nome,idade,empresa,funcao} = req.body;
+        const query = conection("users");
         
-        try{
-            await conection('user').insert({
+        try {
+            await query.insert({
                 nome,
                 idade,
                 empresa,
                 funcao,
-                })
-
-        }catch(erro){ 
-            console.log(erro)
+            })
+            res.json(nome);
+        }catch(erro){
+            console.log(erro);
         }
-        res.json(nome);
+
     },
     
     async receber(req,res){
-        res.json({"Aa":"aaaa"})
+        try{
+            const data = await conection("users").select('*')
+            res.json(data)
+        }catch(erro){
+            console.log(erro);
+        }
+
     }
 
 }
